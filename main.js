35b34fcc80ee1f5505de2b7c8fc3aebac8e607ad
SHREYAAS1000 <75013816+SHREYAAS1000@users.noreply.github.com>
2021-05-26 16:19:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+
+Webcam.set({
+width:350,
+height:300,
+image_format: "png",
+png_quality : 90
+});
+
+camera  =  document.getElementById("camera");
+Webcam.attach("#camera");
+
+
+function takesnapshot()
+{
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log("ml5 version",ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/ZHNn8dXvu/model.json",modelLoaded) //imageClassifier used to to import the model from teachadle manchen
+
+function modelLoaded(){
+    console.log("Model is loaded!")
+}
+var prediction_1 = "";
+var prediction_2 = "";
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is" + prediction_1;
+    speak_data_2 = "The second prediction is" + prediction_2;
+utterthis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2)
+synth.speak(utterthis);
+}
+
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotResult); // classify is a function used to do the comparision 
+}
+function gotResult(error,results)
+{
+if (error)
+{
+    console.error(error);
+}
+else{
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML = results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+
+    speak() // calling the the function speak
+
+    if (results[0].label == "victory")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#9996;"
+    }
+    if (results[0].label == "amazing")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#128076;"
+    }
+    if (results[0].label == "best")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#128077;"
+    }
+
+   
+}
+}
